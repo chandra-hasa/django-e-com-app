5b03c4ded7ab902a5526ba58ca6d7f1b7f4753d6
chandra-hasa <65844280+chandra-hasa@users.noreply.github.com>
2021-06-14 16:12:17 +0530

    form actions added

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 from django.core.paginator import Paginator
 # Create your views here.
 
@@ -22,4 +22,17 @@ def detail(request, id):
 
 
 def checkout(request):
+    if request.method == "POST":
+        items = request.POST.get('items', '')
+        name = request.POST.get('name', "")
+        email = request.POST.get('email', "")
+        address = request.POST.get('address', "")
+        city = request.POST.get('city', "")
+        state = request.POST.get('state', "")
+        zipcode = request.POST.get('zipcode', "")
+
+        order = Order(items=items, name=name, email=email, address=address,
+                      city=city, state=state, zipcode=zipcode)
+        order.save()
+
     return render(request, 'shop/checkout.html')
